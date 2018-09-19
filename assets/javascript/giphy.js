10f5ad31c9f49349ba2fbf8db3457bcbc6939d1b
LealTX <jacob.leal32@gmail.com>
2018-09-19 17:49:46 -0500

    display my index & show stills

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    var topics = ["batman", "superman", "joker", "darkseid", "godzilla", "predator", "terminator"]
+    var topics = ["the batman", "the superman", "joker", "darkseid", "godzilla", "predator", "terminator"]
 
     // Display Buttons
     function showButtons() {
@@ -8,15 +8,59 @@ $(document).ready(function () {
 
         topics.forEach(function (val, i) {
             var topic = topics[i];
-            console.log(topic);
+            topicNoSpace = topic.split(" ").join("+");
+            // console.log(topicNoSpace);
             var newButton = $("<button>");
-            newButton.addClass("topic");
-            newButton.attr("data-name",topic);
+            newButton.addClass("topic-btn");
+            newButton.attr("data-name",topicNoSpace);
             newButton.text(topic);
             $("#buttons-view").append(newButton);
 
         });
     }
+    
+    // Display Gifs when click
+    function displayGifs() {
+               
+        var searchTopic = $(this).attr("data-name");
+        var apiURL = "https://api.giphy.com/v1/gifs/search?q="+ searchTopic +"&api_key=beDexIJohUGNq0cNMKoin2Vlmfhsn9Xc&limit=10";
+        console.log(apiURL);
+
+        $.ajax({
+            url: apiURL,
+            method:"GET"
+        }).then(function(response) {
+            console.log(response);
+            $("#giphy-view").empty();
+
+            var giphyDiv = $("<div class='newGiphy'>");
+            
+            var giphyLength = response.data.length;
+            console.log(giphyLength);
+
+            for(var i = 0; i < giphyLength; i++){
+                var newRow = $("<tr>");
+                var indDiv = $("<div class='single'>");
+                var grabRating = response.data[i].rating;
+                var pRating = $("<h5>").text("Rating: "+grabRating);
+                console.log(pRating);
+                
+                var grabStill = response.data[i].images.fixed_height_small_still.url;
+                var stillImage = $("<img class='singleImage'>").attr("src", grabStill);
+
+                indDiv.prepend(stillImage);
+                newRow.prepend(pRating);
+                indDiv.prepend(newRow);
+                giphyDiv.append(indDiv);
+
+                $("#giphy-view").append(giphyDiv);
+            }
+        })
+    }
+
+    // Run Function to Show Giphys
+    $(document).on("click",".topic-btn",displayGifs);
+    $(document).on("click",".singleImage",displayGifs);
 
     showButtons();
 
